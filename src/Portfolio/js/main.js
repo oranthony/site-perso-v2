8538313959fd5fb820a1f3ee9b2f4b74b3aac1e4
Anthony Loroscio <anthonyloroscio@gmail.com>
2018-06-24 23:34:27 +0200

    Work on portfolio

--- src/Portfolio/js/main.js
+++ src/Portfolio/js/main.js
@@ -1,24 +1,26 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        /*menu: '#menu',
-        lockAnchors: false,
-        anchors:['treasure', 'delpeche'],*/
-        verticalCentered: false
+        //menu: '#menu',
+        //lockAnchors: false,
+        anchors:['1', '2', '3', '4', '5'],
+        verticalCentered: true,
+        /*navigation: true,
+        navigationPosition: 'right'*/
     });
 
 
-    var $head = $(".embed").contents().find("body");
+    /*var $head = $(".embed").contents().find("body");
     $head.append('style="overflow: hidden"');
 
     $("embed").documentElement.style.overflow = 'hidden';
 
-    //$(".embed").delay(3000).css("overflow", "hidden");
+    //$(".embed").delay(3000).css("overflow", "hidden");*/
 
 });
 
-bioMp(document.getElementById('preview'), {
+/*bioMp(document.getElementById('preview'), {
     url: 'http://m.labo-delpech.com',
     view: 'front',
     image: 'img/iphone6_front_black.png',
     height: 500
-});
+});*/
